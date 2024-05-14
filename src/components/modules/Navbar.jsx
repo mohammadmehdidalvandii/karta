043cb8118e24c419d2b0589d6bfae85893a727af
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-14 16:29:06 +0330

    create section navbar-responsive

--- src/components/modules/Navbar.jsx
+++ src/components/modules/Navbar.jsx
@@ -1,36 +1,83 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import style from './Navbar.module.css';
 import Link from 'next/link';
-import { FaUser } from "react-icons/fa";
+import { FaUser , FaBars , FaTimes  } from "react-icons/fa";
 
 
 function Navbar() {
-  return (
-   <section className={style.navBar}>
-        <div className="container">
-            <div className={style.navbar_wrapper}>
-                    <ul className={style.navbar_menu}>
-                        <li className={style.navbar_item}>
-                            <Link href='' className={style.navbar_item_link}>خانه</Link>
-                        </li>
-                        <li className={style.navbar_item}>
-                            <Link href='' className={style.navbar_item_link}>قوانین</Link>
-                        </li>
-                        <li className={style.navbar_item}>
-                            <Link href='' className={style.navbar_item_link}>تماس باما</Link>
-                        </li>
-                        <li className={style.navbar_item}>
-                            <Link href='' className={style.navbar_item_link}>پرسش ها</Link>
-                        </li>
-                    </ul>
+    const [showMenu , setShowMenu] = useState(false);
 
+    // show-menu 
+    const handlerShowMenu = ()=>{
+        setShowMenu(!showMenu)
+    }
+  return (
+    <>
+        <section className={`${style.navBar} ${"d-none d-md-flex"}`}>
+                <div className="container">
+                    <div className={style.navbar_wrapper}>
+                            <ul className={style.navbar_menu}>
+                                <li className={style.navbar_item}>
+                                    <Link href='' className={style.navbar_item_link}>خانه</Link>
+                                </li>
+                                <li className={style.navbar_item}>
+                                    <Link href='' className={style.navbar_item_link}>قوانین</Link>
+                                </li>
+                                <li className={style.navbar_item}>
+                                    <Link href='' className={style.navbar_item_link}>تماس باما</Link>
+                                </li>
+                                <li className={style.navbar_item}>
+                                    <Link href='' className={style.navbar_item_link}>پرسش ها</Link>
+                                </li>
+                            </ul>
+
+                            <Link href='' className={style.navbar_loginRegister}>
+                                <span className={style.navbar_loginRegister_icon}><FaUser/></span>
+                                <span className={style.navbar_loginRegister_text}>ثبت نام / ورود</span>
+                            </Link>
+                    </div>
+                </div>
+        </section>
+
+        <section className={`${style.navbarRes} ${"d-block d-md-none"}`}>
+            <div className="container">
+                <div className={style.navbarRes_wrapper}>
+                    {showMenu ? (
+                           <button className={style.navbarRes_btn} onClick={handlerShowMenu}>
+                           <FaTimes />
+                       </button>
+                    ) :(
+                        <button className={style.navbarRes_btn} onClick={handlerShowMenu}>
+                        <FaBars/>
+                    </button>
+                    )}
+                   
+                           <div className={showMenu ? style.navbarRes_bg_active :style.navbarRes_bg}>
+                           <ul className={style.navbarRes_menu}>
+                               <li className={style.navbarRes_item}>
+                                   <Link href='' className={style.navbarRes_item_link}>خانه</Link>
+                               </li>
+                               <li className={style.navbarRes_item}>
+                                   <Link href='' className={style.navbarRes_item_link}>قوانین</Link>
+                               </li>
+                               <li className={style.navbarRes_item}>
+                                   <Link href='' className={style.navbarRes_item_link}>تماس با ما</Link>
+                               </li>
+                               <li className={style.navbarRes_item}>
+                                   <Link href='' className={style.navbarRes_item_link}>پرسش ها</Link>
+                               </li>
+                           </ul>
+                       </div>
+                 
                     <Link href='' className={style.navbar_loginRegister}>
-                        <span className={style.navbar_loginRegister_icon}><FaUser/></span>
-                        <span className={style.navbar_loginRegister_text}>ثبت نام / ورود</span>
-                    </Link>
+                                <span className={style.navbar_loginRegister_icon}><FaUser/></span>
+                                <span className={style.navbar_loginRegister_text}>ثبت نام / ورود</span>
+                            </Link>
+                </div>
             </div>
-        </div>
-   </section>
+        </section>
+    </>
   )
 }
 
