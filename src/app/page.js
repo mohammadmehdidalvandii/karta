f55bbc8346aeaee15e0f4d8619a5252380243057
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-16 14:35:31 +0330

    create section produts for home-page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '@/components/modules/Navbar/Navbar'
 import Banner from '@/components/template/home/banner/Banner'
+import Product from '@/components/template/home/Product/Product'
 
 function page() {
   return (
     <>
       <Navbar/>
       <Banner/>
+      <Product/>
     </>
   )
 }
