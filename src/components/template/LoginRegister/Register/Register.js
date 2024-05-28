480bb702840804a134956d16c08b5f23237b76f9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-28 11:58:29 +0330

    create signup create form successfully

--- src/components/template/LoginRegister/Register/Register.js
+++ src/components/template/LoginRegister/Register/Register.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Register.module.css';
+import { showSwal } from '@/utils/helpers';
+import { validationEmail, validationPassword, validationPhone } from '@/utils/auth';
 
 function Register() {
+    const [email , setEmail] = useState("");
+    const [name , setName] = useState("");
+    const [phone , setPhone] = useState("");
+    const [password , setPassword] = useState("");
+    const [rePassword , setRePassword] = useState("");
+  
+    const signUp = async (event)=>{
+        event.preventDefault();
+        if(!name.trim()){
+            return showSwal("نام اجباری است","error","تلاش مجدد");
+        } 
+         if(name.length < 4){
+            showSwal("نام کاربری حداقل 4 حرف","error","تلاش مجدد")
+        }
+
+        const isValidPhone = validationPhone(phone);
+        if(!isValidPhone){
+            return showSwal("شماره تماس وارد شد معتبر نیست","error","تلاش مجدد")
+        }
+
+        const isValidEmail = validationEmail(email);
+        if(!isValidEmail){
+            return showSwal("ایمیل وارد شد معتبر نیست ","error","تلاش مجدد")
+        }
+
+        const isValidPassword = validationPassword(password ,rePassword);
+        if(!isValidPassword){
+            return showSwal("رمزعبور وارد شد قابل حدس هست","error","تلاش مجدد")
+        }
+
+        if(password !== rePassword ){
+            return showSwal("رمزعبور هماهنگ نیست","error","تلاش مجدد")
+        }
+
+        const user = {name , phone , email , password};
+
+        const res = await fetch("/api/auth/signup",{
+            method:"POST",
+            headers:{
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+
+        if(res.status === 201){
+            showSwal("ثبت نام با موفقیت انجام شد","success","ورود به پنل کاربری")
+        } else if(res.status === 422){
+            showSwal("کاربر با این اطلاعات از قبل وجود دارد","error","تلاش مجدد")
+        }
+    }
+
+
   return (
   <form action="#" className={style.register}>
         <div className='loginRegister_title_text'>
@@ -10,25 +64,50 @@ function Register() {
         </div>
         <div className="formBox">
             <span className="formBox_text">ایمیل</span>
-            <input type="text" className="formBox_input" />
+            <input
+             type="email"
+              className="formBox_input"
+              value={email}
+              onChange={(event)=>setEmail(event.target.value)}
+              />
         </div>
         <div className="formBox">
             <span className="formBox_text">نام کاربری</span>
-            <input type="text" className="formBox_input" />
+            <input
+             type="text" 
+             className="formBox_input" 
+             value={name}
+             onChange={(event)=>setName(event.target.value)}
+             />
         </div>
         <div className="formBox">
             <span className="formBox_text">شماره همراه</span>
-            <input type="text" className="formBox_input" />
+            <input
+             type="text" 
+             className="formBox_input" 
+             value={phone}
+             onChange={(event)=>setPhone(event.target.value)}
+             />
         </div>
         <div className="formBox">
             <span className="formBox_text">رمزعبور</span>
-            <input type="text" className="formBox_input" />
+            <input
+             type="password" 
+             className="formBox_input" 
+             value={password}
+             onChange={(event)=>setPassword(event.target.value)}
+             />
         </div>
         <div className="formBox">
             <span className="formBox_text">تکرار رمز عبور</span>
-            <input type="text" className="formBox_input" />
+            <input
+             type="password" 
+             className="formBox_input" 
+             value={rePassword}
+             onChange={(event)=>setRePassword(event.target.value)}
+             />
         </div>
-        <button className="formSubmit">ارسال</button>
+        <button className="formSubmit" onClick={signUp}>ارسال</button>
   </form>
   )
 }
