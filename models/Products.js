8b00f32e140e3f358081154880203afec055b6fe
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-30 10:50:17 +0330

    create model products

---
+++ models/Products.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+    },
+    price:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required: true,
+    }
+});
+
+const model = mongoose.models.Products || mongoose.model("Products", schema);
+export default model
