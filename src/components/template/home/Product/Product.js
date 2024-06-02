b37fa0d8e5e3f2bef15cfc8c411111901c415811
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-02 11:56:35 +0330

    update Product-page for home-page

--- src/components/template/home/Product/Product.js
+++ src/components/template/home/Product/Product.js
@@ -18,27 +18,6 @@ function Product({categories}) {
                     <Card name={category.name} title={category.title}/>
                 </div>
                     )) }
-                {/* <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div>
-                <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                    <Card/>
-                </div> */}
             </div>
         </div>
     </section>
