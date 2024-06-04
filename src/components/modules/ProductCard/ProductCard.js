31aa7e0586024d16a16ac5e8c59b62dd24a0576f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-04 11:26:43 +0330

    button addToBasket style for ProductCard

--- src/components/modules/ProductCard/ProductCard.js
+++ src/components/modules/ProductCard/ProductCard.js
@@ -3,11 +3,12 @@ import style from './ProductCard.module.css';
 
 function ProductCard({name , price}) {
   return (
-    <button className={style.productCard}>
+    <div className={style.productCard}>
+      <button className={style.ProductCard_addToBasket}>افزودن به سبد </button>
         <img src="/images/itunes.jpg" alt="product img" className={style.productCard_img} />
         <h6 className={style.productCard_name}>{name}</h6>
         <span className={style.productCard_price}>{price} دلاری</span>
-    </button>
+    </div>
   )
 }
 
