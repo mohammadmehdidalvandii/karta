19ce2f163f806c05c449204df07ecded84645529
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-04 10:37:11 +0330

    fixed style ProductCard

--- src/components/modules/ProductCard/ProductCard.js
+++ src/components/modules/ProductCard/ProductCard.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import style from './ProductCard.module.css';
-import Link from 'next/link';
 
 function ProductCard({name , price}) {
   return (
-    <Link href='/' className={style.productCard}>
+    <button className={style.productCard}>
         <img src="/images/itunes.jpg" alt="product img" className={style.productCard_img} />
         <h6 className={style.productCard_name}>{name}</h6>
         <span className={style.productCard_price}>{price} دلاری</span>
-    </Link>
+    </button>
   )
 }
 
