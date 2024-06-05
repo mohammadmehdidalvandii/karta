4f44a16b3628a7de1b98e8c3ac922e519fce741c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-05 12:39:49 +0330

    add product to basket and totalPrice

--- src/components/template/shopping/Basket.js
+++ src/components/template/shopping/Basket.js
@@ -1,15 +1,27 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import style from './Basket.module.css';
 import Link from 'next/link';
+import { set } from 'mongoose';
 
 function Basket() {
     const [cart ,setCart] = useState([]);
+    const [totalPrice ,  setTotalPrice] = useState(0);
 
-    // useEffect(()=>{
-    //     const localCart = JSON.parse(localStorage.getItem("cart")|| []);
-    //     setCart(localCart)
-    // },[])
+    useEffect(()=>{
+        const localCart = JSON.parse(localStorage.getItem("cart"));
+        if(localCart ){
+            setCart(localCart)
+        }else{
+            setCart([])
+        }
+    },[])
+
+    useEffect(()=>{
+        const priceTotal = cart.reduce((acc , item)=> acc + item.price *59000 , 0)
+        setTotalPrice(priceTotal)
+    },[cart])
+
   return (
     <section className={style.basket}>
         <div className="container">
@@ -36,40 +48,38 @@ function Basket() {
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12">
                     <div className={style.basket_wrapper}>
-                        {/* {cart.length == [] && (
-                                <div className={style.basket_empty}>
+                        {cart.length == [] && (
+                            <div className={style.basket_empty}>
                                     <h5 className={style.basket_empty_text}>سبد خرید خالی است </h5>
                                     <Link href='/' className={style.basket_empty_link}>محصولات</Link>
                                 </div>
-                        )} */}
-                        <ul className={style.basket_items}>
+                        )}
+                        {cart.map(item=>(
+                            <ul className={style.basket_items}>
+                            <h4 className={style.basket_alert}>تمام مبالغ به صورت اتوماتیک به تومان تبدیل میشود</h4>
                             <li className={style.basket_item}>
                                 <img src="/images/itunes.jpg" alt="product img" className={style.basket_item_img} />
                             </li>
                             <li className={style.basket_item}>
-                                <span className={style.basket_item_title}>گیفت کارت itunes</span>
+                                <span className={style.basket_item_title}>{item.name} - {item.price} دلاری</span>
                             </li>
                             <li className={style.basket_item}>
                                 <button className={style.basket_item_btn}>حذف</button>
                             </li>
                         </ul>
-                        <ul className={style.basket_items}>
-                            <li className={style.basket_item}>
-                                <span className={style.basket_item_txt}>جمع کل</span>
-                            </li>
-                            <li className={style.basket_item}>
-                                <span className={style.basket_item_text}>دلار - 100</span>
-                            </li>
-                        </ul>
-                        <ul className={style.basket_items}>
+                        ))}
+                                    <ul className={style.basket_items}>
                             <li className={style.basket_item}>
                                 <span className={style.basket_item_txt}>قابل پرداخت</span>
                             </li>
                             <li className={style.basket_item}>
-                                <span className={style.basket_item_text}>5,900,000 - تومان</span>
+                                <span className={style.basket_item_text}>{totalPrice.toLocaleString()} - تومان</span>
                             </li>
                         </ul>
+                        <ul className={style.basket_items}>
                             <button className={style.basket_pay}>پرداخت</button>
+                        </ul>
+                    
                     </div>
                 </div>
             </div>
