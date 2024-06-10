b9ba51bdc67ec7ff4d94dab10709bdfadb643b41
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-10 09:57:27 +0330

    create section order for user-page

--- src/app/User/page.js
+++ src/app/User/page.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import style from '../../styles/User.module.css';
 import Dashboard from '@/components/template/user/Dashboard';
+import Order from '@/components/template/user/Order';
 
 function page() {
     const [activeMenu , setActiveMenu] = useState("Dashboard");
@@ -34,7 +35,7 @@ function page() {
                     </div>
                     <div className="col-lg-9 col-md-9 col-sm-12">
                         {activeMenu === "Dashboard" && (<Dashboard/>)}
-                        {activeMenu === "Order" && (<h1>Order</h1>)}
+                        {activeMenu === "Order" && (<Order/>)}
                         {activeMenu === "info" && (<h1>info</h1>)}
                     </div>
                 </div>
---
+++ src/components/template/user/Order.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import style from './Order.module.css';
+import Pheader from '@/components/modules/P-header/P-header';
+
+function Order() {
+  return (
+    <section className={style.order}>
+        <div className="row">
+            <div className="col-12">
+                <Pheader text="سفارشات"/>
+            </div>
+        </div>
+        <div className="row mt-4">
+            <div className="col-12">
+                <div className={style.order_wrapper}>
+                <table className={style.order_table}>
+                        <thead className={style.order_table_thead}>
+                            <th>#</th>
+                            <th>شماره پیگیری</th>
+                            <th>نام/خانوادگی</th>
+                            <th>کوپن</th>
+                            <th>مبلغ</th>
+                            <th>محصول</th>
+                            <th>وضعیت</th>
+                            <th>تاریخ</th>
+                        </thead>
+                        <tbody className={style.order_table_tbody}>
+                            <tr className={style.order_table_tr}>
+                                <th>1</th>
+                                <th>#123446</th>
+                                <th>محمدمهدی</th>
+                                <th>-</th>
+                                <th>864,000</th>
+                                <th>itunes</th>
+                                <th>پرداخت شد</th>
+                                <th>22 / 03 /1403</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Order
